9ace71e1242696beb87ec486b013e6cb5f1c97b6
ilhamgusti <ilham.wibowo51@gmail.com>
2022-01-02 23:22:54 +0700

    cimple connection create table

--- database/connect.go
+++ database/connect.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
+	"time"
 	"todo-apis-go/config"
 
 	"gorm.io/driver/mysql"
@@ -17,32 +19,36 @@ func ConnectDB() {
 	var err error
 	var dsn string
 
-	// if !fiber.IsChild() {
-	// fmt.Println("I'm the parent process")
 	dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/?&parseTime=True&charset=utf8&loc=Local", config.Config("MYSQL_USER"), config.Config("MYSQL_PASSWORD"), config.Config("MYSQL_HOST"), 3306)
-	DB, err = gorm.Open(mysql.New(mysql.Config{
-		DSN:                       dsn,
-		DefaultStringSize:         100,
-		DisableDatetimePrecision:  true,
-		DontSupportRenameIndex:    true,
-		DontSupportRenameColumn:   true,
-		DontSupportForShareClause: true,
-		SkipInitializeWithVersion: true,
-	}), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
+		fmt.Println(err)
 		panic("failed to connect database")
 	}
+	defer db.Close()
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + config.Config("MYSQL_DBNAME"))
+	if err != nil {
+		panic(err)
+	}
 
-	DB.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", config.Config("MYSQL_DBNAME")))
-	DB.Exec(fmt.Sprintf("USE %s", config.Config("MYSQL_DBNAME")))
-	DB.Exec("CREATE TABLE IF NOT EXISTS `todos` (`id` int AUTO_INCREMENT,`title` varchar(50),`activity_group_id` int unsigned,`is_active` boolean,`priority` varchar(10),`created_at` datetime(3) NULL,`updated_at` datetime(3) NULL,`deleted_at` datetime(3) NULL,PRIMARY KEY (`id`),INDEX idx_todos_activity_group_id (`activity_group_id`),INDEX idx_todos_deleted_at (`deleted_at`))")
-	DB.Exec("CREATE TABLE IF NOT EXISTS `activities` (`id` int AUTO_INCREMENT,`email` varchar(50),`title` varchar(50),`created_at` datetime(3) NULL,`updated_at` datetime(3) NULL,`deleted_at` datetime(3) NULL,PRIMARY KEY (`id`),INDEX idx_activities_deleted_at (`deleted_at`))")
+	_, err = db.Exec("USE " + config.Config("MYSQL_DBNAME"))
+	if err != nil {
+		panic(err)
+	}
 
-	sqlDB, _ := DB.DB()
-	sqlDB.Close()
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS `todos` (`id` int AUTO_INCREMENT,`title` varchar(50),`activity_group_id` int unsigned,`is_active` boolean,`priority` varchar(10),`created_at` datetime(3) NULL,`updated_at` datetime(3) NULL,`deleted_at` datetime(3) NULL,PRIMARY KEY (`id`),INDEX idx_todos_activity_group_id (`activity_group_id`),INDEX idx_todos_deleted_at (`deleted_at`))")
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS `activities` (`id` int AUTO_INCREMENT,`email` varchar(50),`title` varchar(50),`created_at` datetime(3) NULL,`updated_at` datetime(3) NULL,`deleted_at` datetime(3) NULL,PRIMARY KEY (`id`),INDEX idx_activities_deleted_at (`deleted_at`))")
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Close()
+	if err != nil {
+		panic(err)
+	}
 
 	dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?&parseTime=True&charset=utf8&loc=Local", config.Config("MYSQL_USER"), config.Config("MYSQL_PASSWORD"), config.Config("MYSQL_HOST"), 3306, config.Config("MYSQL_DBNAME"))
 	DB, err = gorm.Open(mysql.New(mysql.Config{
@@ -54,10 +60,19 @@ func ConnectDB() {
 		DontSupportForShareClause: true,
 		SkipInitializeWithVersion: true,
 	}), &gorm.Config{
-		Logger:                 logger.Default.LogMode(logger.Info),
-		SkipDefaultTransaction: true,
-		PrepareStmt:            true,
+		Logger:                                   logger.Default.LogMode(logger.Silent),
+		SkipDefaultTransaction:                   true,
+		PrepareStmt:                              true,
+		DisableAutomaticPing:                     true,
+		DisableNestedTransaction:                 true,
+		DisableForeignKeyConstraintWhenMigrating: true,
 	})
+
+	sqlDB, _ := DB.DB()
+	sqlDB.SetConnMaxIdleTime(time.Hour)
+	sqlDB.SetConnMaxLifetime(24 * time.Hour)
+	sqlDB.SetMaxIdleConns(100)
+	sqlDB.SetMaxOpenConns(200)
 
 	if err != nil {
 		panic("failed to connect database")
